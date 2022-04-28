e9afa13d17b9b0b8a96dfa6a98062bf274549519
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-29 01:27:34 +0800

    Add url

--- rustic.el
+++ rustic.el
@@ -5,6 +5,7 @@
 ;;
 ;; Keywords: languages
 ;; Package-Requires: ((emacs "26.1") (rust-mode "1.0.3") (dash "2.13.0") (f "0.18.2") (let-alist "1.0.4") (markdown-mode "2.3") (project "0.3.0") (s "1.10.0") (seq "2.3") (spinner "1.7.3") (xterm-color "1.6"))
+;; URL: https://github.com/brotzeit/rustic
 
 ;; This file is distributed under the terms of both the MIT license and the
 ;; Apache License (version 2.0).
